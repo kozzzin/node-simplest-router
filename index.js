796ddb9a78e80489b9d02af64d96ad452a8d1599
Vitalii Kozin <v.kozzzin@gmail.com>
2022-11-13 13:04:50 +0100

    express realization

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+const express = require('express');
 
+const app = express();
 
-const server = http.createServer((req,res) => {
+let data = '';
+const menu = '<ul><li><a href="/">index</a></li><li><a href="/about">about</a></li><li><a href="/contacts">contacts</a></li></ul>';
 
-  const url = req.url;
-  let data = '';
-  const menu = '<ul><li><a href="/">index</a></li><li><a href="/about">about</a></li><li><a href="/contacts">contacts</a></li></ul>';
+app.use(express.static(path.join(__dirname,'pages')));
 
-  let statusCode = false;
-
-  switch(url) {
-    case '/':
-      data = '<head><title>Index</title></head>' + menu + '<p>I\'m index!</p>';
-      break;
-    case '/about':
-    case '/about/':
-      data = '<head><title>About</title></head>' + menu + '<p>I\'m about page!</p>';
-      break;
-    case '/contacts':
-    case '/contacts/':
-      data = '<head><title>Contacts</title></head>' + menu + '<p>I\'m contacts page!</p>';
-      break;
-    default:
-      data = '<head><title>404: not found</title></head>' + menu + '<p>Not found. error 404</p>';
-      statusCode = 404;
-      break;
+const people = [
+  {
+    "name": "game",
+    "score": 25
+  },
+  {
+    "name": "psn",
+    "score": 125
   }
+];
 
-  console.log('hi there');
-  res.statusCode = statusCode || 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write(data);
-  // console.log('env',process.env);
-  res.end();
+app.get('/game/people',(req,res) => {
+  res.json(people);
+})
+
+app.get('/',(req,res) => {
+  res.set()
+  // res.send( '<head><title>Index</title></head>' + menu + '<p>I\'m index page!</p>');
+  console.log(path.join(__dirname,'pages','index.html'));
+  res.sendFile(path.join(__dirname,'pages','index.html'));
 });
+
+app.get('/about',(req,res) => {
+  res.send( '<head><title>About</title></head>' + menu + '<p>I\'m about page!</p>');
+});
+
+app.get('/contacts',(req,res) => {
+  res.send('<head><title>Contacts</title></head>' + menu + '<p>I\'m contacts page!</p>');
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('<head><title>404: not found</title></head>' + menu + '<p>Not found. error 404</p>')
+})
+
+
 
 const port = process.env.port || 5000;
 
-server.listen(port, (err) => {
+app.listen(port, (err) => {
   if (err) { 
     console.log(err);
+    return;
   }
   console.log('server is here');
 })
